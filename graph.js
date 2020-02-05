a83fc7db99149ba7080216150f35a0530432676b
Draguve <ritwikpatil1998@gmail.com>
2020-02-05 11:39:42 +0000

    Escape to clear

--- graph.js
+++ graph.js
@@ -13,6 +13,19 @@ var drawRect = draw.getBoundingClientRect(); // get the bounding rectangle
 
 searchBox.oninput = searchBoxValueChanged;
 searchBox.onfocus = searchBoxValueChanged;
+searchBox.onkeydown = function(evt) {
+    evt = evt || window.event;
+    var isEscape = false;
+    if ("key" in evt) {
+        isEscape = (evt.key === "Escape" || evt.key === "Esc");
+    } else {
+        isEscape = (evt.keyCode === 27);
+    }
+    if (isEscape) {
+        searchBox.value = ""
+        searchBoxValueChanged();
+    }
+};
 
 var state = {
     selectedNode: null,
